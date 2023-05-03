e7fd77c7e0e12d004ce6d945808dabe17f92561e
Steven Lubbe <130697880+Stevelub@users.noreply.github.com>
2023-05-03 16:05:14 +0200

    Delete f_hyperion directory

--- f_hyperion/f_mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-app_name = 'f_mysite'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-]
